7bef54f33e802664a8400c61074b0ae92d032517
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-05-14 20:42:18 +1000

    The first pagination option

--- goods/urls.py
+++ goods/urls.py
@@ -6,5 +6,6 @@ app_name = "goods"
 
 urlpatterns = [
     path("<slug:category_slug>/", views.index, name="index"),
+    path("<slug:category_slug>/<int:page>/", views.index, name="index"),
     path("product/<slug:product_slug>/", views.product, name="product"),
 ]
