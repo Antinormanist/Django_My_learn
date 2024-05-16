d2c7cf1a33cca2c18e2195bc7c7997f09978d9b4
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-05-16 16:40:56 +1000

    Modife pagination using getquery

--- goods/urls.py
+++ goods/urls.py
@@ -6,6 +6,5 @@ app_name = "goods"
 
 urlpatterns = [
     path("<slug:category_slug>/", views.index, name="index"),
-    path("<slug:category_slug>/<int:page>/", views.index, name="index"),
     path("product/<slug:product_slug>/", views.product, name="product"),
 ]
