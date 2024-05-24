1f558469d91ee880cb4c8e5e88eeb4d6a73cf649
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-05-25 06:32:59 +1000

    Full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,14 +1,10 @@
 from django.db.models import Q
 from goods.models import Products
+from django.contrib.postgres.search import SearchVector, SearchRank
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    q = Q()
-    for token in query.split():
-        if len(token) > 2:
-            q |= Q(name__icontains=token)
-            q |= Q(description__icontains=token)
-
-    return Products.objects.filter(q)
+    
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
