4d50645b4cc4ade9d070b0d7f0bfdb8a6c4282f8
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-06-01 06:34:16 +1000

    Add registration form

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def login(request):
@@ -27,18 +27,28 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+        
     context = {
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form,
     }
     return render(request, 'users/registration.html', context)
 
 
 def profile(request):
     context = {
-        'title': 'Home - Кабинет'
+        'title': 'Home - Кабинет',
     }
     return render(request, 'users/profile.html', context)
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
